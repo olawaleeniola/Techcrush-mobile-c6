bcc24f7e70b3c03fea70f7a76c4de85b0a9aafe0
olawaleeniola <olawaleeniola348@gmail.com>
2026-04-02 20:02:56 +0100

    Styling Done

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,70 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Button } from '@react-navigation/elements';
+import { useState } from 'react';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+  const [greeting, setGreeting] = useState('Welcome')
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+  const updateGreeting = () => {
+    if (greeting === 'Welcome') {
+      setGreeting('Goodbye');
+    } else {
+      setGreeting('Welcome');
+    }
+  };
+
+  return (
+    <SafeAreaView style={ style.container }>
+      <ScrollView>
+        <Image
+          source={{ uri: 'https://cdn3.toonboom.com/wp-content/uploads/2025/05/13095403/Asset.png' }}
+          style={{ width: 400, height: 500 }}
+        />
+
+        <View style={style.Box}>
+          <Text style={style.Text}>{greeting} Techcrush 1</Text>
+        </View>
+
+        <Button onPress={updateGreeting}>Click me</Button>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
+
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+  
 });
+
+const style = StyleSheet.create(
+
+  {
+    container:{
+      backgroundColor:'white',
+      height:'100%',
+      padding:20,
+    },
+    Box:{
+      borderRadius:10,
+      backgroundColor:'blue',
+      padding:20,
+      margin:20,
+      alignItems:'center',
+    },
+    Text:{
+      color:'white',
+      fontSize:20,
+      textAlign:'center',
+      fontWeight:'bold',
+    }
+  
+    
+  }
+)
---
+++ styles/main.ts
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+const Style = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    height: '100%',
+    padding: 20,
+  },
+  Box: {
+    borderRadius: 10,
+    backgroundColor: 'blue',
+    padding: 20,
+    margin: 20,
+    alignItems: 'center',
+  },
+  Text: {
+    color: 'white',
+    fontSize: 20,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+})
+
+export default Style;
